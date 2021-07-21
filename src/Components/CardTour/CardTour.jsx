9cd062fca8f3cf9fc9194e0a5a1008a174177a3f
romelx23 <romelx23@gmail.com>
2021-07-21 16:22:17 -0500

    add style tours page

---
+++ src/Components/CardTour/CardTour.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './CardTour.css'
+
+export const CardTour = () => {
+    return (
+        <div className="card__tour">
+            <img src="https://picsum.photos/seed/picsum/200/300" alt="place" />
+            <h4>Mountains</h4>
+            <h5>Bring me there</h5>
+        </div>
+    )
+}
--- src/Components/Screens/Tours/Tours.jsx
+++ src/Components/Screens/Tours/Tours.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
-
+import './Tours.css'
+// import logo from '../../../../public/assets/images/mapa.png'
+import image from '../../../images/mapa.png'
+import { CardTour } from '../../CardTour/CardTour'
+import { InputSearch } from '../../InputSearch/InputSearch'
 export const Tours = () => {
     return (
-        <div>
+        <div className="container__tours">
             <h2>Tours</h2>
+            <InputSearch/>
+            <div className="content__tours">
+                <img src={image} alt="map" />
+                <div className="content_cards">
+                    <CardTour/>
+                    <CardTour/>
+                    <CardTour/>
+                    <CardTour/>
+                    <CardTour/>
+                    <CardTour/>
+                    <CardTour/>
+                </div>
+            </div>
         </div>
     )
 }
